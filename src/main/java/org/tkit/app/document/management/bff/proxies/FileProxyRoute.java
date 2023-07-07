3d020927a850655d4e289ca5015f28ed74fc7024
alwin-crasto <alwin.crasto@gmail.com>
2023-07-07 18:39:28 +0530

    Fix for the null proxyPath issue at runtime.

--- src/main/java/org/tkit/app/document/management/bff/proxies/FileProxyRoute.java
+++ src/main/java/org/tkit/app/document/management/bff/proxies/FileProxyRoute.java
@@ -6,7 +6,6 @@ import javax.enterprise.event.Observes;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.tkit.quarkus.log.cdi.LogExclude;
 
-import io.quarkus.logging.Log;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientOptions;
@@ -16,21 +15,21 @@ import lombok.extern.slf4j.Slf4j;
 @ApplicationScoped
 @Slf4j
 public class FileProxyRoute {
-    @ConfigProperty(name = "dms.file-upload.proxy-path")
-    private static String proxyPath;
-
+    private final String proxyPath;
     private final String baseUrl;
     private final HttpClient httpClient;
 
     FileProxyRoute(
             Vertx vertx,
-            @ConfigProperty(name = "documentmgmt/mp-rest/url") String processUrl) {
+            @ConfigProperty(name = "documentmgmt/mp-rest/url") String processUrl,
+            @ConfigProperty(name = "dms.file-upload.proxy-path") String path) {
         this.baseUrl = processUrl;
         this.httpClient = vertx.createHttpClient(new HttpClientOptions());
+        this.proxyPath = path;
     }
 
     public void init(@Observes @LogExclude Router router) {
-        Log.info("Adding proxy route for logo upload endpoint path " + proxyPath + " with a proxy forward to " + baseUrl);
+        log.info("Adding proxy route for logo upload endpoint path {} with a proxy forward to {}", proxyPath, baseUrl);
         router.route(proxyPath).handler(ProxyHandler.create(httpClient, baseUrl));
     }
 
